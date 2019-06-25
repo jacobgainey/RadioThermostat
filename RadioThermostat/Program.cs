46a8a338def8b6da24e819fe6ff28f6d40c3f118
Jacob Gainey <jgainey@carolina.rr.com>
2019-06-24 21:29:11 -0400

    Initial check-in of module RadioThermostat

---
+++ RadioThermostat/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RadioThermostat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RadioThermostat");
+            Task.Run(() => RadioThermostat.RunAsync().GetAwaiter().GetResult());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RadioThermostat/RadioThermostatModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RadioThermostat
+{
+    public class SystemInformation
+    {
+        public string uuid { get; set; }
+        public int api_version { get; set; }
+        public string fw_version { get; set; }
+        public string wlan_fw_version { get; set; }
+    }
+
+    public class Model
+    {
+        public string model { get; set; }
+    }
+    
+}
